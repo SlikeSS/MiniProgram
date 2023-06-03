e011487ad59e271f77c9db7a827a094e3bec60de
SlikeSS <antonsundukov@mail.ru>
2023-06-03 22:09:40 +0400

    Добавлено возвращение к началу + догадки для для добавления индексов

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static boolean repeat = true;
+
     public static void main(String[] args) {
+        while (repeat) {
+            core();
+        }
+    }
+
+    private static void core() {
         System.out.println("1 - добавить значения в массив\n" +
                 "2 - удалить значения в массив\n" +
                 "3 - изменить значения в массив\n" +
@@ -10,43 +18,56 @@ public class Main {
                 "* - выход");
         Scanner a = new Scanner(System.in);
         String b = a.nextLine();
-        String c = "*";
-        if (b == "1" | b == "2" |b == "3" | b == "4") {
-            switch (b) {
-                case "1":
-                    dobavit();
-                    break;
-                case "2":
-                    udalit();
-                    break;
-                case "3":
-                    izmenit();
-                    break;
-                case "4":
-                    prosmotr();
-                    break;
-            }
+        switch (b) {
+            case "1":
+                add();
+                break;
+            case "2":
+                delete();
+                break;
+            case "3":
+                update();
+                break;
+            case "4":
+                view();
+                break;
+            case "*":
+                exit();
+                break;
         }
-        else if (c.equals(b)){
-            System.exit(0);
-        }
-        else {
-            System.out.println("Введено неверное значение");
-        }
+        System.out.println("Введено неверное значение");
     }
-    private static void dobavit() {
+
+    public static void view() {
+        System.out.println("Вид массива:");
+        ArrayList<Boolean> arrayList = new ArrayList<Boolean>();
+        System.out.println(arrayList);
+        System.out.println("Всего элементов:"+arrayList.size());}
+
+    public static void add() {
         System.out.println("Введите 0 или 1");
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        if (b == 0) {
+            arrayList.add(false);
+            System.out.println(arrayList);
+        } else if (b == 1) {
+            arrayList.add(true);
+            System.out.println(arrayList);
+        } else {
+            System.out.println("Неверное значение");
+        }
     }
-    private static void udalit() {
+    public static void delete() {
         System.out.println("Введите индекс в массив для его удаления");
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
     }
-    private static void izmenit() {
+    public static void update() {
         System.out.println("Введите индекс в массиве для его изменения");
     }
-    private static void prosmotr(){
-        System.out.println("Вид массива:");
-        boolean[] array = new boolean[5];
-        System.out.println(Arrays.toString(array));
-
-    }
+    public static void exit()
+    {System.out.println("Выход из программы");
+        System.exit(0);
+}
 }
