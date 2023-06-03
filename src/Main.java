d69fd68d40e88e4cae21cb9d38127bc03b116b97
Anton <antonsundukov@mail.ru>
2023-06-03 11:13:58 +0400

    Построение базовой логики с текстом

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("1 - добавить значения в массив\n" +
+                "2 - удалить значения в массив\n" +
+                "3 - изменить значения в массив\n" +
+                "4 - вывод массива\n" +
+                "* - выход");
+        Scanner a = new Scanner(System.in);
+        String b = a.nextLine();
+        String c = "*";
+        if (b == "1" | b == "2" |b == "3" | b == "4") {
+            switch (b) {
+                case "1":
+                    dobavit();
+                    break;
+                case "2":
+                    udalit();
+                    break;
+                case "3":
+                    izmenit();
+                    break;
+                case "4":
+                    prosmotr();
+                    break;
+            }
         }
+        else if (c.equals(b)){
+            System.exit(0);
+        }
+        else {
+            System.out.println("Введено неверное значение");
+        }
+    }
+    private static void dobavit() {
+        System.out.println("Введите 0 или 1");
+    }
+    private static void udalit() {
+        System.out.println("Введите индекс в массиве для его удаления");
+    }
+    private static void izmenit() {
+        System.out.println("Введите индекс в массиве для его изменения");
+    }
+    private static void prosmotr(){
+        System.out.println("Вид массива:");
+        boolean[] array = new boolean[5];
+        System.out.println(Arrays.toString(array));
     }
 }
