99b78a494b9fe028e4f8cd6ce8c8024cf1ea8c49
SlikeSS <antonsundukov@mail.ru>
2023-06-05 19:44:25 +0400

    Добавление + удаление, решена проблема с концом кейса

--- src/Main.java
+++ src/Main.java
@@ -19,31 +19,31 @@ public class Main {
                 "* - выход");
         Scanner a = new Scanner(System.in);
         String b = a.nextLine();
-        switch (b) {
-            case "1":
-                add();
-                break;
-            case "2":
-                delete();
-                break;
-            case "3":
-                update();
-                break;
-            case "4":
-                view();
-                break;
-            case "*":
-                exit();
-                break;
+            switch (b) {
+                case "1":
+                    add();
+                    break;
+                case "2":
+                    delete();
+                    break;
+                case "3":
+                    update();
+                    break;
+                case "4":
+                    view();
+                    break;
+                case "*":
+                    exit();
+                    break;
+                default:
+                    System.out.println("Введено неверное значение");
+            }
         }
-        System.out.println("Введено неверное значение");
-    }
-
     private static void view() {
         System.out.println("Вид массива:");
         System.out.println(arrayList);
         System.out.println("Всего элементов:"+arrayList.size());
-    return;}
+   }
 
     private static void add() {
         System.out.println("Введите 0 или 1");
@@ -51,10 +51,10 @@ public class Main {
         int b = a.nextInt();
         if (b == 0) {
             arrayList.add(false);
-            System.out.println(arrayList);
+            System.out.println("Теперь массив: " + arrayList);
         } else if (b == 1) {
             arrayList.add(true);
-            System.out.println(arrayList);
+            System.out.println("Теперь массив: "+ arrayList);
         } else {
             System.out.println("Неверное значение");
             while (repeat) {
@@ -65,12 +65,27 @@ public class Main {
     }
     private static void delete() {
         System.out.println("Введите индекс в массив для его удаления");
+        System.out.println("Существующие индексы от 1 до " + arrayList.size());
         Scanner a = new Scanner(System.in);
-        int b = a.nextInt();
-
+        int b = a.nextInt()-1;
+        if (arrayList.size()>b){
+            arrayList.remove(b);
+        System.out.println("Теперь массив имеет форму: " + arrayList);}
+        else if (arrayList.size()==0)
+        {
+            System.out.println("Массив пуст");
+        }
+        else {
+            System.out.println("Не существует такого индекса");
+        }
     }
+
     private static void update() {
         System.out.println("Введите индекс в массиве для его изменения");
+        System.out.println("Существующие индексы от 1 до " + arrayList.size());
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt()-1;
+
     }
     private static void exit()
     {System.out.println("Выход из программы");
