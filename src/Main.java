f830ae244c612ee126ec881506b718ee4b8abee6
SlikeSS <antonsundukov@mail.ru>
2023-06-06 21:11:59 +0400

    Добавлено 0 и 1, + рабочий неоптимизированный 3 пункт

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static ArrayList<Boolean> arrayList = new ArrayList<Boolean>();
-    static boolean repeat = true;
 
     public static void main(String[] args) {
-        while (repeat) {
+        while (true) {
             core();
         }
     }
-
     private static void core() {
         System.out.println("1 - добавить значения в массив\n" +
-                "2 - удалить значения в массив\n" +
-                "3 - изменить значения в массив\n" +
+                "2 - удалить значения в массиве\n" +
+                "3 - изменить значения в массиве\n" +
                 "4 - вывод массива\n" +
                 "* - выход");
-        Scanner a = new Scanner(System.in);
-        String b = a.nextLine();
-            switch (b) {
+        Scanner scanCore = new Scanner(System.in);
+        String caseCore = scanCore.nextLine();
+            switch (caseCore) {
                 case "1":
                     add();
                     break;
@@ -41,36 +40,41 @@ public class Main {
         }
     private static void view() {
         System.out.println("Вид массива:");
-        System.out.println(arrayList);
-        System.out.println("Всего элементов:"+arrayList.size());
-   }
-
-    private static void add() {
-        System.out.println("Введите 0 или 1");
-        Scanner a = new Scanner(System.in);
-        int b = a.nextInt();
-        if (b == 0) {
-            arrayList.add(false);
-            System.out.println("Теперь массив: " + arrayList);
-        } else if (b == 1) {
-            arrayList.add(true);
-            System.out.println("Теперь массив: "+ arrayList);
-        } else {
-            System.out.println("Неверное значение");
-            while (repeat) {
-                add();
-                return;
+        for (int i = 0; i < arrayList.size(); i++) {
+            Boolean b = arrayList.get(i);
+            if (b == true) {
+                System.out.print("1 ");
+            }
+            else {
+                System.out.print("0 ");
             }
         }
-    }
+        System.out.println("Всего элементов:"+arrayList.size());
+   }
+    private static void add() {
+        System.out.println("Введите 0 или 1");
+        Scanner scanAdd = new Scanner(System.in);
+        int inputAdd = scanAdd.nextInt();
+            if (inputAdd == 0) {
+                arrayList.add(false);
+            }
+            else if (inputAdd == 1) {
+                arrayList.add(true);
+            } else {
+                System.out.println("Неверное значение");
+                while (true) {
+                    add();
+                    return;
+                }
+            }
+            }
     private static void delete() {
         System.out.println("Введите индекс в массив для его удаления");
-        System.out.println("Существующие индексы от 1 до " + arrayList.size());
-        Scanner a = new Scanner(System.in);
-        int b = a.nextInt()-1;
-        if (arrayList.size()>b){
-            arrayList.remove(b);
-        System.out.println("Теперь массив имеет форму: " + arrayList);}
+        System.out.println("Существующие индексы: " + arrayList.size());
+        Scanner scanDelete = new Scanner(System.in);
+        int inputDelete = scanDelete.nextInt()-1;
+        if (arrayList.size()>inputDelete){
+            arrayList.remove(inputDelete);}
         else if (arrayList.size()==0)
         {
             System.out.println("Массив пуст");
@@ -79,16 +83,64 @@ public class Main {
             System.out.println("Не существует такого индекса");
         }
     }
-
     private static void update() {
         System.out.println("Введите индекс в массиве для его изменения");
-        System.out.println("Существующие индексы от 1 до " + arrayList.size());
+        System.out.println("Количество существующих индексов: " + arrayList.size());
         Scanner a = new Scanner(System.in);
-        int b = a.nextInt()-1;
-
-    }
+        int b = a.nextInt() - 1;
+        if (arrayList.size() <= b) {
+            System.out.println("Не существует такого индекса\n" +
+                    "Введите ещё раз");
+            int d = a.nextInt() - 1;
+            if (arrayList.size() <= d) {
+                System.out.println("Не существует такого индекса");
+                System.out.println("Введите 0 или 1");
+                Scanner e = new Scanner(System.in);
+                int f = e.nextInt();
+                if (f == 0) {
+                    arrayList.add(false);
+                    System.out.println("Значение добавлено");
+                }
+                else if (f == 1) {
+                    arrayList.add(true);
+                    System.out.println("Значение добавлено");
+                }
+                else {
+                    System.out.println("Неверное значение");
+                }
+            }
+            else if (arrayList.size()==0){
+                System.out.println("Массив пуст");}
+            else{
+                    System.out.println("Выбран " + (d+1) + " индекс");
+                    System.out.println("Меняем на 0 или 1?");
+                    int z = a.nextInt();
+                    if (z==0){
+                        arrayList.set(z,false);
+                        System.out.println("Значение индекса изменено на 0");}
+                    else{arrayList.set(z,true);
+                        System.out.println("Значение индекса изменено на 1");
+                }
+            }
+        }
+        else if (arrayList.size()==0){
+        System.out.println("Массив пуст");}
+        else
+        {System.out.println("Выбран " + (b+1) + " индекс");
+            System.out.println("Меняем на 0 или 1?");
+            int d = a.nextInt();
+            if (d==0){
+            arrayList.set(b,false);
+            System.out.println("Значение индекса изменено на 0");}
+            else{arrayList.set(b,true);
+                System.out.println("Значение индекса изменено на 1");}
+            }
+        }
     private static void exit()
     {System.out.println("Выход из программы");
         System.exit(0);
+}
+private static void change(){
+
 }
 }
